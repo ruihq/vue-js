06fbef9bc12060c9247204c3081d5664baf93ac8
Lunime ૮ ˶ᵔ ᵕ ᵔ˶ ა <120704613+ruinaigelreogo@users.noreply.github.com>
2022-12-21 06:31:26 -0800

    An simple Liking feature created in vue.js!

---
+++ index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <button @click="incrementLikes">Like</button>
+      <span>{{ likes }}</span>
+    </div>
+</template>
+
+<template>
+    <div>
+      <liking-component v-bind:likes="article.likes"></liking-component>
+    </div>
+  </template>
+  
+  <script>
+  import LikingComponent from './LikingComponent.vue';
+  
+  export default {
+    components: {
+      LikingComponent
+    },
+    data() {
+      return {
+        article: {
+          likes: 0
+        }
+      }
+    }
+  }
+  </script>
